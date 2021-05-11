f30daa53ad7944166b5f5a419e39d6f93d6ca7e4
Jay0810 <jay.jiang23@sitechhs.com>
2021-05-11 17:29:26 -0400

    sadfkf

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
 const query = async function () {
   try {
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon-species/pikachu/`
-    );
+    const response = await fetch(`https://pokeapi.co/api/v2/stat/`);
     const data = await response.json();
     console.log(data.results);
   } catch (error) {
