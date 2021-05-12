99e2c6b1f65d96eb1d15b89d0c2b5e6c73b26212
LeoC05 <71610558+LeoC05@users.noreply.github.com>
2021-05-11 22:08:52 -0400

    pics

---
+++ src/js/DOM.js
@@ -0,0 +1,5 @@
+const DOMSelectors = {
+    grid: document.querySelector(".poke-grid"),
+};
+
+export { DOMSelectors };
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,17 @@
+import { DOMSelectors } from "./DOM";
+
+
 const query = async function () {
   try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1118`);
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=898`);
     const data = await response.json();
-    console.log(data);
+    data.results.forEach((pokemon) => {
+      DOMSelectors.grid.insertAdjacentHTML("beforeend", 
+      `<div class="poke-card">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.results.indexOf(pokemon) + 1}.png" alt="">
+        <h1 class="poke-name">${pokemon.name}</h1>
+      </div>`)
+    })
   } catch (error) {
     console.log(error);
     alert("Hey something went wrong");
