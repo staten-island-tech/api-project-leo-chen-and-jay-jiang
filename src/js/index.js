6adc64bb88fd6ca3832f3bfee5dc5d6b6c8890f7
Jay0810 <jay.jiang23@sitechhs.com>
2021-05-24 23:56:43 -0400

    dnaskldnasdinjsah kj

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,20 @@
 import { DOMSelectors } from "./DOM";
 
-
 const query = async function () {
   try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=898`);
     const data = await response.json();
     data.results.forEach((pokemon) => {
-      DOMSelectors.grid.insertAdjacentHTML("beforeend", 
-      `<div class="poke-card">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.results.indexOf(pokemon) + 1}.png" alt="">
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="poke-card">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          data.results.indexOf(pokemon) + 1
+        }.png" alt="">
         <h1 class="poke-name">${pokemon.name}</h1>
-      </div>`)
-    })
+      </div>`
+      );
+    });
   } catch (error) {
     console.log(error);
     alert("Hey something went wrong");
