642999a3ae4c8adc4ef2efa4403bd123ddf87d06
Jay0810 <jay.jiang23@sitechhs.com>
2021-05-24 22:45:44 -0400

    fadsfasd

---
+++ src/js/search.js
@@ -0,0 +1,5 @@
+const DOMselectors = {
+  grid: document.querySelector("poke-grid"),
+  searchForm: document.getElementById("search-form"),
+  searchArea: document.getElementById("search-area"),
+};
