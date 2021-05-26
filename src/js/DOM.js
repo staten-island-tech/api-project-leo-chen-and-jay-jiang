c013d082969ef37a29020de58fe56ebb6fb424a8
LeoC05 <71610558+LeoC05@users.noreply.github.com>
2021-05-26 01:47:39 -0400

    ?????????

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,7 +1,7 @@
 const DOMSelectors = {
     grid: document.querySelector(".poke-grid"),
-    searchForm: document.getElementById("search-form"),
-    searchArea: document.getElementById("search-area"),
+    searchForm: document.getElementById("search-form"), 
+    searchArea: document.getElementById("search-area"), //text they typed in
 };
 
 export { DOMSelectors };
