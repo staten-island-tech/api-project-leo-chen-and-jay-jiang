e11a45f1cf5aeb1bdb2432684de01d3055f52e97
LeoC05 <71610558+LeoC05@users.noreply.github.com>
2021-05-26 01:34:35 -0400

    info

--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,18 @@ const query = async function () {
   try {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=898`);
     const data = await response.json();
+    console.log(data);
     data.results.forEach((pokemon) => {
+      //const pokeType = fetch(`https://pokeapi.co/api/v2/type/${pokemon.name}`);
+      //const pokeValue = Promise.resolve(pokeType);
+      //console.log(pokeType);
       DOMSelectors.grid.insertAdjacentHTML(
         "beforeend",
         `<div class="poke-card">
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
           data.results.indexOf(pokemon) + 1
         }.png" alt="">
-        <h1 class="poke-name">${pokemon.name}</h1>
+        <h7 class="poke-name">${pokemon.name}</h7>
       </div>`
       );
     });
